af0b314303d548edb4d1cb18791b8994f0970e38
ychento <yiyaochen@gmail.com>
2023-09-04 17:38:59 -0400

    add Doc folder for screenshot

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -95,47 +95,6 @@ const useApplicationData = () => {
     dispatch({ type: actionTypes.SET_PHOTO_SELECTED, photo });
   };
 
-  // const useApplicationData = () => {
-  //   const [state, setState] = useState({
-  //     isModalOpen: false,
-  //     modalPhoto: null,
-  //     favouritePhotos: [],
-  //   });
-
-  //   const onClosePhotoDetailsModal = () => {
-  //     setState({
-  //       ...state,
-  //       isModalOpen: false,
-  //     });
-  //   };
-
-  //   const updateToFavPhotoIds = (photoId) => {
-  //     if (state.favouritePhotos.includes(photoId)) {
-  //       setState((prev) => {
-  //         const filteredPhotos = prev.favouritePhotos.filter((item) => item !== photoId);
-  //         return {
-  //           ...prev,
-  //           favouritePhotos: filteredPhotos,
-  //         };
-  //       });
-  //     } else {
-  //       setState((prev) => {
-  //         return {
-  //           ...prev,
-  //           favouritePhotos: [...prev.favouritePhotos, photoId],
-  //         };
-  //       });
-  //     }
-  //   };
-
-  //   const setPhotoSelected = (id) => {
-  //     setState({
-  //       ...state,
-  //       isModalOpen: true,
-  //       modalPhoto: photos[id],
-  //     });
-  //   };
-
   return {
     state,
     onClosePhotoDetailsModal,
